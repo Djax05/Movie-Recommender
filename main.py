dc80b91afbaa59805a4dbd21ce0c4462312dbc30
Makinde John <mackjohn341@gmail.com>
2025-11-06 22:37:46 +0100

    Finetuned the model using a hierarchical cluster model, and created a recommendation function

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import pandas as pd
+
+
+data_sample = pd.read_csv("data/data_sample.csv")
+
+
+def recommend_within_cluster(title):
+    title = title.strip().lower()
+    data_sample['movie_title_clean'] = data_sample['movie_title'].str.strip().str.lower()
+
+    matches = data_sample[data_sample['movie_title_clean'] == title]
+    if matches.empty:
+        return f"No movie found for '{title}'. Try a different title."
+
+    cluster = matches['cluster_id'].values[0]
+    similar_movies = data_sample[data_sample['cluster_id'] == cluster]['movie_title'].tolist()
+    similar_movies.remove(matches['movie_title'].values[0])
+    return similar_movies
+
+
 def main():
     print("Hello from movie-recommender!")
+    print(recommend_within_cluster("John Carter"))
 
 
 if __name__ == "__main__":
