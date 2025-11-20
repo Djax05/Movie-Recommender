7e5a0890518422f5318eb695df6abed1c5b8484c
Makinde John <mackjohn341@gmail.com>
2025-11-20 19:57:27 +0100

    Added feature weighting to the parameters used in the faiss index

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import numpy as np
+import pandas as pd
 import faiss
 import json
 
-embeddings = np.load("data/sentence_embeddings.npy").astype("float32")
-faiss.normalize_L2(embeddings)
+sentence_embeddings = np.load("data/sentence_embeddings.npy").astype("float32")
+encoded_data = pd.read_csv("data/encoded_data.csv").values.astype("float32")
+numeric_scaled = np.load("data/numeric_scaled.npy").astype("float32")
+
+w_sentence = 0.8
+w_genre = 0.5
+w_numeric = 0.2
+
+sentence_w = sentence_embeddings * w_sentence
+genre_w = encoded_data * w_genre
+numeric_w = numeric_scaled * w_numeric
+
+full_features = np.hstack([
+    sentence_w,
+    genre_w,
+    numeric_w
+])
+
+faiss.normalize_L2(full_features)
 
 index = faiss.read_index("data/faiss_index.bin")
 
@@ -23,7 +41,17 @@ def recommend(title, k=10):
 
     movie_id = title_to_index[title]
 
-    query_vec = embeddings[movie_id].reshape(1, -1)
+    query_sentence = sentence_embeddings[movie_id]
+    query_genre = encoded_data[movie_id]
+    query_numeric = numeric_scaled[movie_id]
+    query_vec = np.hstack([
+        query_sentence * w_sentence,
+        query_genre * w_genre,
+        query_numeric * w_numeric
+    ]).astype("float32")
+
+    query_vec = query_vec.reshape(1, -1)
+
     faiss.normalize_L2(query_vec)
 
     distances, indices = index.search(query_vec, k + 1)
--- scripts/build_index.py
+++ scripts/build_index.py
@@ -1,16 +1,33 @@
 import faiss
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import normalize
 
 
-X = np.load("data/sentence_embeddings.npy").astype("float32")
+sentence_embeddings = np.load("data/sentence_embeddings.npy").astype("float32")
+encoded_data = pd.read_csv("data/encoded_data.csv").astype("float32")
+numeric_scaled = np.load("data/numeric_scaled.npy").astype("float32")
 
-faiss.normalize_L2(X)
+w_sentence = 0.8
+w_genre = 0.5
+w_numeric = 0.2
 
-dim = X.shape[1]
 
-index = faiss.IndexFlatIP(dim)
+sentence_w = sentence_embeddings * w_sentence
+genre_w = encoded_data.values * w_genre
+numeric_w = numeric_scaled * w_numeric
 
-index.add(X)
+full_features = np.hstack([
+    sentence_w,
+    genre_w,
+    numeric_w
+])
+
+full_features_norm = normalize(full_features)
+
+index = faiss.IndexFlatIP(full_features_norm.shape[1])
+
+index.add(full_features_norm.astype("float32"))
 
 faiss.write_index(index, "data/faiss_index.bin")
 
